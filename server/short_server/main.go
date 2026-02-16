668d7bbc8ae21ee5133fabf0a5d252e82e19ff2c
behery_ahmed <behery.ahmed1991@gmail.com>
2026-02-16 10:34:54 +0300

    add GET route 'test'

--- server/short_server/main.go
+++ server/short_server/main.go
@@ -1,5 +1,43 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
+	"os"
 
+	"github.com/danielgtaylor/huma/v2"
+	"github.com/danielgtaylor/huma/v2/adapters/humagin"
+	"github.com/gin-gonic/gin"
+
+	_ "github.com/danielgtaylor/huma/v2/formats/cbor"
+)
+
+type HelloOutput struct {
+	Body struct {
+		Message string `json:"message"`
+	}
+}
+
+func main() {
+	r := gin.New()
+	r.Use(gin.Logger(), gin.Recovery())
+
+	apiGroup := r.Group("/api")
+
+	config := huma.DefaultConfig("My API", "1.0.0")
+	config.Servers = []*huma.Server{
+		{URL: "/api"},
+	}
+
+	api := humagin.NewWithGroup(r, apiGroup, config)
+
+	huma.Get(api, "/hello", func(ctx context.Context, in *struct{}) (*HelloOutput, error) {
+		out := &HelloOutput{}
+		out.Body.Message = "hello"
+		return out, nil
+	})
+	if err := r.Run(":8080"); err != nil {
+		log.Printf("server failed to start: %v", err)
+		os.Exit(1)
+	}
 }
